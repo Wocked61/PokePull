7c2b6627c0f99d34a0b20646789c7cd9a573045f
Wocked61 <DylanP@csu.fullerton.edu>
2025-05-19 23:24:49 -0700

    added a background to the app folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import backgroundImage from './assets/Box_Forest_background2.png';
 
 function App() {
   return (
+    <div style={{ 
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      minHeight: '100vh'
+    }}>
     <div className="app-container">
       <h1>Welcome to PokéPull</h1>
       <p>This is the main PokéPull app page where you'll be able to view and open Pokémon card packs.</p>
@@ -16,6 +23,7 @@ function App() {
           <p>Show recent card pulls here</p>
         </div>
       </div>
+    </div>
     </div>
   );
 }
