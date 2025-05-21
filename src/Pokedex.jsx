6b83dc1acc39b98c8eb95e8d288ec252db768848
Wocked61 <DylanP@csu.fullerton.edu>
2025-05-20 23:18:05 -0700

    fixed the background of the pokedex

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import './Pokedex.css';
 import backgroundImage from './assets/Pokedex_BG.png';
 
-
 function Pokedex() {
   return (
-    <div className="pokedex-container" style={{ 
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      minHeight: '100vh',
-      width: '100%',
-    }}>
-      <h1>Pokedex</h1>
-      <p>Explore the world of Pokemon!</p>
+    <div className="pokedex-page">
+      <div className="pokedex-background" style={{ 
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        minHeight: '100vh',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: -1,
+      }}></div>
+      <div className="pokedex-container">
+        <h1>Pokedex</h1>
+        <p>Explore the world of Pokemon!</p>
+      </div>
     </div>
   );
 }
