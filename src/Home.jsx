9d405371b44c16b37bfbaafc4ba153aebe89da2f
Wocked61 <DylanP@csu.fullerton.edu>
2025-05-20 23:31:30 -0700

    added an improved background for the homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
-import backgroundImage from './assets/Wigglytuff.png';
+import backgroundImage from './assets/Home_BG!.png';
 
 function Home() {
   return (
-    <div className="home-container" style={{ 
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      minHeight: '100vh'
-    }}>
-      <h1>Welcome to PokéPull</h1>
-      <p>This is the Home PokéPull app page where</p>
-      <div className="placeholder-content">
-
-        <div className="placeholder-box" onClick={() => window.location.href = '/App#/App'}>
-          <h2>Start Pulling</h2>
-          <p>Spin for Pokemon here</p>
+    <div className="home-page">
+      <div className="home-background" style={{ 
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        minHeight: '100vh'
+      }}>
+      
+      <div className="home-container">
+        <div className='placeholder-box-title'>
+        <h1>Welcome to PokéPull</h1>
+        <p>This is the Home PokéPull app page</p>
         </div>
-
-        <div className="placeholder-box" onClick={() => window.location.href = '/App#/Pokedex'}>
-          <h2>PokeDex</h2>
-          <p>See all available Pokemon here</p>
+        <div className="placeholder-content">
+          <Link to="/App" className="link-no-style">
+            <div className="placeholder-box">
+              <h2>Start Pulling</h2>
+              <p>Spin for Pokemon here</p>
+            </div>
+          </Link>
+          
+          <Link to="/Pokedex" className="link-no-style">
+            <div className="placeholder-box">
+              <h2>PokeDex</h2>
+              <p>See all available Pokemon here</p>
+            </div>
+          </Link>
         </div>
-
       </div>
     </div>
-
+    </div>
   );
 }
 
--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -9,18 +9,14 @@ function Pokedex() {
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
         width: '100%',
-        minHeight: '100vh',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        zIndex: -1,
-      }}></div>
+        minHeight: '100vh'
+      }}>
       <div className="pokedex-container">
         <h1>Pokedex</h1>
         <p>Explore the world of Pokemon!</p>
       </div>
+    </div>
     </div>
   );
 }
